7ce0c6fe56aea6d41d965e87e82291cddaa337aa
niteesh <a.nithishkumarreddy922@gmail.com>
2024-01-31 18:05:55 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -11,8 +11,27 @@ def equijoin(request):
     EO=Emp.objects.select_related('deptno').filter(deptno__dloc='Dallas')
     EO=Emp.objects.select_related('deptno').filter(ename__in=('Allen','Scott'))
     EO=Emp.objects.select_related('deptno').filter(job__in=('Salesman','Manager'))
-
-
-
+    
     d={'EO':EO}
     return render(request,'equijoin.html',d)
+
+
+def selfjoin(request):
+    EMO=Emp.objects.select_related('mgr').all()
+    EMO=Emp.objects.select_related('mgr').filter(mgr__ename='King')
+    EMO=Emp.objects.select_related('mgr').filter(sal__gt=3000)
+    EMO=Emp.objects.select_related('mgr').filter(mgr__sal__gt=3000)
+    EMO=Emp.objects.select_related('mgr').filter(sal__lt=3000)
+    EMO=Emp.objects.select_related('mgr').filter(mgr__sal__lt=3000)
+    EMO=Emp.objects.select_related('mgr').filter(deptno=30)
+    EMO=Emp.objects.select_related('mgr').filter(comm=None)
+    EMO=Emp.objects.select_related('mgr').filter(hiredate__year=1980)
+    EMO=Emp.objects.select_related('mgr').filter(mgr__hiredate__year=1981)
+    EMO=Emp.objects.select_related('mgr').filter(ename__in=('Allen','Scott'))
+    EMO=Emp.objects.select_related('mgr').filter(mgr__ename__in=('King','Scott'))
+    EMO=Emp.objects.select_related('mgr').filter(job='Salesman')
+    EMO=Emp.objects.select_related('mgr').filter(mgr__job='Manager')
+    
+
+    d={'EMO':EMO}
+    return render(request,'selfjoin.html',d)
